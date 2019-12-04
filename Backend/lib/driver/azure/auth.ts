437c62e94c32a75ca4aad272731c96593bc3eef2
jz <huangjz97@outlook.com>
2019-12-04 14:31:28 -0500

    register finished

--- Backend/lib/driver/azure/auth.ts
+++ Backend/lib/driver/azure/auth.ts
@@ -1,4 +1,4 @@
-import { runQuery } from './azure';
+import { runQuery, runQueryGetOne } from './azure';
 import * as _ from 'lodash';
 //test
 export const getUserInfo = async (username: string, password: string): Promise<IUserInfo> => {
@@ -8,7 +8,7 @@ export const getUserInfo = async (username: string, password: string): Promise<I
 export const insertUserInfo = async (username: string, password: string, email:string): Promise<IAuthResponse> => {
     const getResult = await getUserInfo(username,password);
     if(_.isEmpty(getResult)){
-       return runQuery(`INSERT INTO dbo.users (userName, hashedPassword, email, balance)
+       return runQueryGetOne(`INSERT INTO dbo.users (userName, hashedPassword, email, balance)
        VALUES (\'${username}\', \'${password}\', \'${email}\', 0);`);
     }else{
         return {isRegistered:false} as IAuthResponse;
--- Backend/lib/driver/azure/azure.ts
+++ Backend/lib/driver/azure/azure.ts
@@ -16,3 +16,9 @@ export async function runQuery<T>(query: string): Bluebird<T> {
     const queryResult = await result.query(query);
     return queryResult.recordset[0] as any;
 }
+
+export async function runQueryGetOne<T>(query: string): Bluebird<T> {
+    const result = await new sql.ConnectionPool(client).connect();
+    const queryResult = await result.query(query);
+    return queryResult.recordset as any;
+}
