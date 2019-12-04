b541f5563ae533b1492a30efa8588dcc3cd35ed4
Zishan <liuz6@rose-hulman.edu>
2019-12-03 22:59:38 -0500

    fix return

--- Backend/packages/rbh-api-service/lib/auth.ts
+++ Backend/packages/rbh-api-service/lib/auth.ts
@@ -1,13 +1,11 @@
 import { ApiClient, HttpClient } from './http_client';
 
 export class SessionApi extends ApiClient {
-    public login = async (username: string, password: string) => {
+    public login = async (username: string, password: string) =>
         this.httpClient.request<IUserInfo>('get', `/api/session?username=${username}&password=${password}`);
-    };
 
-    public register = async (registerInfo: IRegisterInfo) => {
-        this.httpClient.request<IAuthResponse, { registerInfo: IRegisterInfo }>('post', '/api/session', { registerInfo });
-    };
+    public register = async (username: string, password: string) =>
+        this.httpClient.request<IUserInfo>('post', `/api/session?username=${username}&password${password}`);
 }
 
 export default (client: HttpClient) => new SessionApi(client);
