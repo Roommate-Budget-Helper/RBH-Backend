3207ffeb5b1e1095140a730ad1898ae785463f0c
Zishan <liuz6@rose-hulman.edu>
2019-12-16 17:36:41 -0500

    fix url

--- Backend/packages/rbh-api-service/lib/home.ts
+++ Backend/packages/rbh-api-service/lib/home.ts
@@ -4,7 +4,7 @@ export class HomeApi extends ApiClient {
     public createHome = async (fullname: string, adminname: string, adminid: Number) =>
         this.httpClient.request<boolean>('post', `/api/home?fullname=${fullname}&adminname=${adminname}&adminid=${adminid}`);
 
-    public getHome = async (userId: number) => this.httpClient.request<IUser2Home[]>('get', `/api/home?${userId}`);
+    public getHome = async (userId: number) => this.httpClient.request<IUser2Home[]>('get', `/api/home?userId=${userId}`);
 }
 
 export default (client: HttpClient) => new HomeApi(client);
