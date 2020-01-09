517f7ab80ab78f364559320aa34518e4c6bc4ef2
Zishan <liuz6@rose-hulman.edu>
2020-01-09 10:31:38 -0500

    vm config

--- Backend/lib/driver/azure/azure.ts
+++ Backend/lib/driver/azure/azure.ts
@@ -2,13 +2,13 @@ import Bluebird from 'bluebird';
 import sql from 'mssql';
 
 export const client = {
-    user: 'rmbhadmin',
-    password: 'Jizhou*Huang',
+    user: 'dev',
+    password: 'devdev',
     database: 'Roommate',
-    server: 'rmbhdw1.database.windows.net',
-    options: {
-        encrypt: true,
-    }
+    server: 'huangj3.csse.rose-hulman.edu'
+    // options: {
+    //     encrypt: true,
+    // }
 };
 
 export async function runQuery<T>(query: string): Bluebird<T> {
