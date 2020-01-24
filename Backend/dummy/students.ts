f45e2b84456918c133f9680719294b06e23d82f3
Darcy <mr.thank@outlook.com>
2020-01-23 20:03:31 -0500

    set up backend test environment

---
+++ Backend/dummy/students.ts
@@ -0,0 +1,17 @@
+export const user = [
+    {
+        id: 1,
+        full_name: 'test1fullName',
+        balance: 0,
+        userName: 'test1userName',
+        hashedPassword: 'aaaaaa'
+    },
+    {
+        id: 2,
+        full_name: 'test2fullName',
+        balance: 0,
+        userName: 'test2userName',
+        hashedPassword: 'bbbbbb'
+    },
+
+]
