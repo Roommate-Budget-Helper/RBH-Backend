34be07f0e5764d482f4e052784f59b51449ffca3
Darcy <mr.thank@outlook.com>
2020-01-30 01:33:57 -0500

    DONE WITH SHARED PLAN

--- Backend/lib/driver/azure/bill.ts
+++ Backend/lib/driver/azure/bill.ts
@@ -20,6 +20,7 @@ const createUser2Bill = async (
         // tslint:disable-next-line: no-floating-promises
         runQuery(`SELECT id FROM dbo.users WHERE userName = \'${roommates[i]}\'`).then(async (result) => {
             userId = (result as IBillCreateResponse).id;
+            console.info("userId", userId, roommates[i], roommates[i].length, result)
             await runQuery(`INSERT INTO dbo.users2bills (billId, userId, proportion, amount, proofFlag, isApproved, proof)
             VALUES (${billId}, ${userId}, ${proportion[i]}, ${amount[i]}, 0, 0, 0)
             SELECT id FROM dbo.bills where id= (SELECT max(id) FROM dbo.bills)`);
@@ -27,7 +28,10 @@ const createUser2Bill = async (
         // tslint:disable-next-line: no-floating-promises
         if (sharePlanid == -1) {
             // tslint:disable-next-line: no-floating-promises
-            runQuery(`INSERT INTO dbo.shareRatioId (sharePlansid, userName, ratio) VALUES (${planId}, ${roommates[i]}, ${proportion[i]})`);
+            runQueryGetOne(`INSERT INTO dbo.shareRatioId (sharePlansid, userName, ratio) VALUES (${planId}, \'${roommates[i]}\', ${proportion[i]})`);
+        }else{
+            runQueryGetOne(`INSERT INTO dbo.shareRatioId (sharePlansid, userName, ratio) VALUES (${sharePlanid}, \'${roommates[i]}\', ${proportion[i]})`);
+
         }
     }
 };
@@ -48,7 +52,9 @@ export const createBill = async (
     created_by:string,
 ): Promise<boolean> => {
     let billId;
-    let planId;
+    let planId:number;
+            // if this is not stored as a shared plan, the plannedSharedFlag from frontend would be 0
+
     if (plannedSharedFlag == 0) {
         // tslint:disable-next-line: no-floating-promises
         runQuery(`INSERT INTO dbo.bills (ownerId, homeId, plannedSharedFlag, totalAmount, isResolved, billName, descri, created_at, created_by)
@@ -58,17 +64,30 @@ export const createBill = async (
             await createUser2Bill(billId, roommates, amount, proportion, 0, 0);
         });
     } else {
+        // if this is a newly created shareplan, the sharePlanid from frontend would be -1
         if (sharePlanid == -1) {
-            planId = await runQuery(`INSERT INTO dbo.sharePlans (full_name, HouseId) VALUES(\'${full_name}\', ${homeId})
-            SELECT id FROM dbo.sharePlans where id= (SELECT max(id) FROM dbo.sharePlans`);
-            billId = await runQuery(`INSERT INTO dbo.bills (ownerId, homeId, plannedSharedFlag, sharePlanid, totalAmount, isResolved, billName, descri, created_at, created_by)
-        VALUES (${ownerId},${homeId},${plannedSharedFlag},${planId},${totalAmount},0, \'${billname}\', \'${billdescri}\', \'${created_at}\', \'${created_by}\')
-        SELECT id FROM dbo.bills where id= (SELECT max(id) FROM dbo.bills)`);
-            console.info(planId, billId)
-        } else {
-            billId = runQueryGetOne(`INSERT INTO dbo.bills (ownerId, homeId, plannedSharedFlag, sharePlanid, totalAmount, isResolved,billName, descri, created_at, created_by)
+            await runQuery(`INSERT INTO dbo.sharePlans (full_name, HouseId) VALUES (\'${full_name}\', ${homeId})
+            SELECT id FROM dbo.sharePlans where id= (SELECT max(id) FROM dbo.sharePlans)`).then(async (planResult)=>{
+                planId = (planResult as IBillCreateResponse).id
+                console.info("planId", planId)
+                await runQuery(`INSERT INTO dbo.bills (ownerId, homeId, plannedSharedFlag, sharePlanid, totalAmount, isResolved, billName, descri, created_at, created_by)
+                VALUES (${ownerId},${homeId},${plannedSharedFlag},${planId},${totalAmount},0, \'${billname}\', \'${billdescri}\', \'${created_at}\', \'${created_by}\')
+                SELECT id FROM dbo.bills where id= (SELECT max(id) FROM dbo.bills)`).then(async (billResult) => {
+                    billId=(billResult as IBillCreateResponse).id
+                    console.info("billId", billId)
+                    await createUser2Bill(billId, roommates, amount, proportion, sharePlanid, planId);
+                })
+            })
+        } 
+        //else it would be a used shareplan
+        else {
+            runQueryGetOne(`INSERT INTO dbo.bills (ownerId, homeId, plannedSharedFlag, sharePlanid, totalAmount, isResolved,billName, descri, created_at, created_by)
         VALUES (${ownerId},${homeId},${plannedSharedFlag},${sharePlanid},${totalAmount},0, \'${billname}\', \'${billdescri}\', \'${created_at}\', \'${created_by}\')
-        SELECT id FROM dbo.bills where id= (SELECT max(id) FROM dbo.bills)`);
+        SELECT id FROM dbo.bills where id= (SELECT max(id) FROM dbo.bills)`).then(async (result)=>{
+            billId=(result as IBillCreateResponse).id
+            await createUser2Bill(billId, roommates, amount, proportion, sharePlanid, 0);
+
+        })
         }
     }
 
@@ -84,7 +103,10 @@ export const getBillByUser = async (userId: numId): Promise<IBill[]> => {
 };
 
 export const deleteBill = async (billid: numId): Promise<Boolean> => {
-    return runQuery(`DELETE FROM dbo.bills WHERE id = ${billid}`);
+    console.log(billid)
+    return runQueryGetOne(`
+    DELETE FROM dbo.users2bills where billId = ${billid}
+    DELETE FROM dbo.bills WHERE id = ${billid}`);
 };
 
 export const markAsResolved = async (billid: numId): Promise<Boolean> => {
@@ -101,11 +123,18 @@ export const getSharePlans = async (houseId: numId): Promise<IBillSharePlan[]> =
     let name = [] as string[];
 
     await runQuery(`SELECT id, full_name from dbo.sharePlans where dbo.sharePlans.HouseId = ${houseId}`).then(async (result) => {
+        if(!result){
+            return sharePlans;
+        }
         (result as IBillSharePlanReturnValue[]).forEach(async (element) => {
             id.push(element.id);
             name.push(element.name);
+            console.log(element)
             await runQuery(`SELECT dbo.shareRatioId.userName, dbo.shareRatioId.ratio FROM dbo.shareRatioId
                 where dbo.shareRatioId.sharePlansid = ${id[id.length - 1]}`).then((ratios) => {
+                    if(!ratios){
+                        return sharePlans;
+                    }
                 (ratios as IBillShareRatioReturnValue[]).forEach(async (pair) => {
                     roommates.push(pair.roommates);
                     prop.push(pair.proportion);
