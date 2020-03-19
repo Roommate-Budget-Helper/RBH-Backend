c351703ce8d412879c3530b663adda1703e5dff8
Darcy <mr.thank@outlook.com>
2020-03-19 18:11:17 -0400

    changes

--- Backend/lib/driver/azure/history.ts
+++ Backend/lib/driver/azure/history.ts
@@ -2,7 +2,7 @@ import { runQuery, runQueryGetOne } from './azure';
 import * as _ from 'lodash';
 //test
 export const getHistory = async (userId: numId): Promise<IHistoryResponse> => {
-    await runQuery(`SELECT billId from dbo.users2bills where userId = ${userId}`).then(async result => {
+    await runQueryGetOne(`SELECT billId from dbo.users2bills where userId = ${userId}`).then(async result => {
         console.info(result)
     })
     return null as unknown as IHistoryResponse
