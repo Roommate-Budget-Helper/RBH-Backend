a30c14c4baf23105d9e4392c6831690b889c9947
Darcy <mr.thank@outlook.com>
2020-03-22 16:30:30 -0400

    changes

--- Backend/packages/rbh-api-service/lib/history.ts
+++ Backend/packages/rbh-api-service/lib/history.ts
@@ -1,9 +1,9 @@
 import { ApiClient, HttpClient } from './http_client';
 
 export class HistoryApi extends ApiClient {
-    public getHistory = async (userId: numId) =>{
+    public getHistory = async (userId: numId) =>
         this.httpClient.request<IHistoryResponse[]>('get', `/api/history?userId=${userId}`);
-    }
+    
 }
 
 export default (client: HttpClient) => new HistoryApi(client);
