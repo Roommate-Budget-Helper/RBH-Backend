047e6a011e4af5a2153f2f7958ee9aa5325f5fa5
Darcy <mr.thank@outlook.com>
2020-04-05 12:51:23 -0400

    added sql injection protection

--- Backend/lib/driver/azure/auth.ts
+++ Backend/lib/driver/azure/auth.ts
@@ -1,8 +1,14 @@
-import { runQuery, runQueryGetOne } from './azure';
+import { runQuery, runQueryGetOne, getConnection } from './azure';
 import * as _ from 'lodash';
+import sql from 'mssql';
+
 //test
 export const getUserInfo = async (username: string, password: string): Promise<IUserInfo | boolean> => {
-    return runQuery(`SELECT * FROM dbo.users WHERE userName= \'${username}\' and hashedPassword= \'${password}\'`);
+    const connection = await getConnection()
+    const request = new sql.Request(connection)
+    request.input('username', sql.VarChar, username)
+    request.input('password', sql.VarChar, password)
+    return (await request.query(`SELECT * FROM dbo.users WHERE userName= @username and hashedPassword= @password`)).recordset[0] as any   ;
 };
 
 export const insertUserInfo = async (username: string, password: string, email: string): Promise<IAuthResponse> => {
